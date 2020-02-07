5eb86b797aa9e87d4f54d677ee43e31ec153fda9
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-02-07 08:52:00 -0600

    Update Grade level.R

--- ISASPDashboard/Grade level.R
+++ ISASPDashboard/Grade level.R
@@ -5,6 +5,9 @@ library(tidyverse)
 
 proficencies <- readr::read_csv("~/PVCSD Annual Data Analysis Packet 2019-2020/data/tidy state proficencies.csv")
 
+
+
+###### GRADE LEVEL PROFICENCIES ######
 grade = 3
 
 proficencies %>%
@@ -13,9 +16,36 @@ proficencies %>%
   mutate(Score = str_remove(Score, pattern = "%")) %>%
   summarise(Score2 = mean(as.numeric(Score), na.rm = T), 
             testName = `Test Name`[1])%>%
-  ggplot(aes(x=Year, y=Score2, fill=testName)) +
+  ggplot(aes(x=Year, y=Score2, fill=testName, color=testName)) +
   geom_bar(stat='identity')+
-  labs(x="", y="", title = paste0("Grade ", grade, " Percent Proficient By Year" ))+
+  ylim(0,100)+
+  labs(x="", y="", 
+       title = paste0("Grade ", grade, " Percent Proficient By Year" ),
+       fill="Test", color= "Test")+
   theme_pleasval()+
   theme(legend.position = "bottom")+
-  scale_fill_manual( values = c("#26547C", "#011638"))
+  scale_fill_manual( values = c("#26547C", "#011638"))+
+  scale_color_manual( values = c("#000000", "#F6F6F6"))
+
+
+###### GRADE LEVEL PROFICENCIES BY SCHOOL ######
+grade = 3
+school = 'BRDV'
+
+proficencies %>%
+  filter(Grade == grade ) %>%
+  group_by(Year, School)%>%
+  mutate(Score = str_remove(Score, pattern = "%")) %>%
+  summarise(Score2 = mean(as.numeric(Score), na.rm = T), 
+            testName = `Test Name`[1])%>%
+  ggplot(aes(x=Year, y=Score2, fill=testName, color=testName)) +
+  geom_bar(stat='identity')+
+  ylim(0,100)+
+  labs(x="", y="", 
+       title = paste0(" Grade ", grade, " Percent Proficient By Year" ), 
+       fill="Test", color= "Test")+
+  theme_pleasval()+
+  theme(legend.position = "bottom")+
+  scale_fill_manual( values = c("#26547C", "#011638"))+
+  scale_color_manual( values = c("#000000", "#F6F6F6"))+
+  facet_wrap(vars(School), nrow = 3)
